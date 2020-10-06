cc10f4981e996564188c31906573e37d1fa5a15a
Jack Wilkinson <jackwilkinson@000225.local>
2020-10-06 18:24:37 +0100

    Follow tutorial https://blog.nrwl.io/using-apollo-graphql-with-react-in-an-nx-workspace-99db8d69cebe to get bones of project

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  projects: [
+    '<rootDir>/apps/api',
+    '<rootDir>/apps/app',
+    '<rootDir>/libs/data-access',
+    '<rootDir>/libs/feature-sets',
+  ],
+};
---
+++ libs/feature-sets/jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  displayName: 'feature-sets',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': [
+      'babel-jest',
+      { cwd: __dirname, configFile: './babel-jest.config.json' },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/feature-sets',
+};
