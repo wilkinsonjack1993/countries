eb38009ef93e92507ce2501795fb69b10d11f0aa
Jack Wilkinson <jackwilkinson@000225.local>
2020-10-07 08:22:18 +0100

    Add base country entity to Graph QL API

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { GraphQLModule } from '@nestjs/graphql';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CountryResolver } from './country.resolver';
 import { SetResolver } from './set.resolver';
 
 @Module({
@@ -13,6 +14,6 @@ import { SetResolver } from './set.resolver';
     })
   ],
   controllers: [AppController],
-  providers: [AppService, SetResolver]
+  providers: [AppService, SetResolver, CountryResolver]
 })
 export class AppModule { }
